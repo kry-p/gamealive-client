23ddc75ae967a050dcfeb1a3f3660c3fd2482a0d
kry-p <jhjung.dev@gmail.com>
2022-01-24 14:02:06 +0900

    Delete local test snippets

--- src/lib/api/client.js
+++ src/lib/api/client.js
@@ -6,6 +6,6 @@ import axios from 'axios';
 const client = axios.create();
 
 // for local test
-client.defaults.baseURL = 'http://localhost:4000';
+// client.defaults.baseURL = 'http://localhost:4000';
 
 export default client;
