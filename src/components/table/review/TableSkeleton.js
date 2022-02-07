abc79f634da0cfd4b4a16769faca46c0f419e0d3
kry-p <jhjung.dev@gmail.com>
2022-02-07 17:51:56 +0900

    Add skeleton element to table
    심의정보 테이블에 로딩 시 스켈레톤을 표시하도록 하여 UX를 개선하였습니다.

--- src/components/table/review/TableSkeleton.js
+++ src/components/table/review/TableSkeleton.js
@@ -5,6 +5,8 @@ import TableHead from '@mui/material/TableHead';
 import TableBody from '@mui/material/TableBody';
 import TableRow from '@mui/material/TableRow';
 import TableCell from '@mui/material/TableCell';
+import Skeleton from '@mui/material/Skeleton';
+
 import Review from './Items';
 
 // table items using redux-saga
@@ -14,7 +16,7 @@ const ReviewItem = ({ review }) => {
 };
 
 // table element
-const TableSkeleton = ({ review, loading }) => {
+export const TableSkeleton = ({ review, loading }) => {
   return (
     <>
       <Table size="small">
@@ -39,4 +41,36 @@ const TableSkeleton = ({ review, loading }) => {
   );
 };
 
-export default TableSkeleton;
+export const TableWhileLoading = () => {
+  const screenWidth = window.innerWidth;
+  const textWidth = window.innerWidth * 0.6;
+  const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  const Dummy = () => {
+    return (
+      <TableRow>
+        <TableCell>
+          <Skeleton width={screenWidth < 384 ? textWidth : 384 * 0.6} />
+        </TableCell>
+        <TableCell>
+          <Skeleton width={25} height={35} />
+        </TableCell>
+      </TableRow>
+    );
+  };
+  return (
+    <Table size="small">
+      <TableHead>
+        <TableRow>
+          <TableCell align={'center'}>게임물명</TableCell>
+          <TableCell align={'center'}>등급</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {array.map((idx) => (
+          <Dummy />
+        ))}
+      </TableBody>
+    </Table>
+  );
+};
